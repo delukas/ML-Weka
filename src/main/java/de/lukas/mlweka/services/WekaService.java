5d742de3a37c119fc407e31ecdd37fdbd3758e7c
Lukas Kotlarski <lukas.kotlarski@gmail.com>
2025-08-25 20:15:35 +0200

    choose RandomForest and cleanup WekaService

--- src/main/java/de/lukas/mlweka/services/WekaService.java
+++ src/main/java/de/lukas/mlweka/services/WekaService.java
@@ -1,7 +1,7 @@
 package de.lukas.mlweka.services;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.trees.RandomForest;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -16,47 +16,96 @@ public class WekaService {
     public static final String TRAIN_DATA_PATH = BASE_PATH + "mnist_train.arff";
     public static final String TEST_DATA_PATH = BASE_PATH + "mnist_test.arff";
     public static final String MODEL_PATH = BASE_PATH + "model.model";
-    private MultilayerPerceptron model;
+
+    private RandomForest model = new RandomForest();
+    private Instances header;
 
     public WekaService() {
+
         File modelFile = new File(MODEL_PATH);
         if(modelFile.exists() && !modelFile.isDirectory()) {
-            System.out.println("Es wurde ein Modell gefunden unter: " + MODEL_PATH);
             loadModel();
             return;
         }
 
         trainModel();
+
     }
 
     public void trainModel(){
 
-        Instances testData = loadData(TRAIN_DATA_PATH);
+        Instances trainData = loadData(TRAIN_DATA_PATH);
+        header = new Instances(trainData, 0);
 
-        testData = new Instances(testData, 0, Math.min(20000, testData.numAttributes()));
-        testData.setClassIndex(testData.numAttributes() - 1);
-
-
-        model = new MultilayerPerceptron();
-        model.setLearningRate(0.025);
-        model.setMomentum(0.5);
-        model.setTrainingTime(500);
-        model.setHiddenLayers("32");
+/*        trainData = new Instances(trainData, 0, Math.min(30000, trainData.numInstances()));
+        trainData.setClassIndex(trainData.numAttributes() - 1);*/
 
         System.out.println("Beginne Training des Modells");
 
         try {
-            model.buildClassifier(testData);
+            model.buildClassifier(trainData);
         } catch (Exception e) {
             throw new RuntimeException("Fehler beim trainieren des Modells: " + e);
         } finally {
             System.out.println("Modell erfolgreich trainiert.");
         }
 
-        System.out.println("Evaluiere Modell");
+
         evaluateModel();
 
-        System.out.println("Speichere Modell");
+        saveModel();
+
+    }
+
+    public void evaluateModel(){
+
+        Instances testData = loadData(TEST_DATA_PATH);
+
+        try {
+            Evaluation evaluation = new Evaluation(testData);
+            evaluation.evaluateModel(model, testData);
+            System.out.println(evaluation.toSummaryString("\n---- Evaluation der Testdaten ----\n", false));
+            System.out.println(evaluation.toClassDetailsString());
+            System.out.println(evaluation.toMatrixString());
+        } catch (Exception e) {
+            throw new RuntimeException("Es konnte keine Evaluation erstellt werden: " + e);
+        }
+
+    }
+
+    public int predictNumber(double[] pixel) {
+
+        Instance instance = new DenseInstance(header.numAttributes());
+        instance.setDataset(header);
+
+        for (int i = 0; i < pixel.length; i++) {
+            instance.setValue(i, pixel[i]);
+        }
+
+        instance.setMissing(header.classIndex());
+
+        try {
+            double prediction = model.classifyInstance(instance);
+            return (int) prediction;
+        } catch (Exception e) {
+            throw new RuntimeException("Bei der Prediction ist ein Fehler aufgetreten: " + e);
+        }
+
+    }
+
+    private void loadModel(){
+
+        try {
+            model = (RandomForest) SerializationHelper.read(MODEL_PATH);
+        } catch (Exception e) {
+            throw new RuntimeException(MODEL_PATH + " konnte nicht gelesen werden.");
+        } finally {
+            System.out.println("Modell geladen: " + MODEL_PATH);
+        }
+
+    }
+
+    private void saveModel() {
 
         try {
             SerializationHelper.write(MODEL_PATH, model);
@@ -65,76 +114,21 @@ public class WekaService {
         } finally {
             System.out.println("Modell gespeichert unter: " + MODEL_PATH);
         }
-    }
 
-    public void evaluateModel(){
-
-        Instances testData = loadData(TEST_DATA_PATH);
-
-        Evaluation evaluation;
-        try {
-            evaluation = new Evaluation(testData);
-        } catch (Exception e) {
-            throw new RuntimeException("Es konnte keine Evaluation erstellt werden: " + e);
-        }
-
-        try {
-            evaluation.evaluateModel(model, testData);
-        } catch (Exception e) {
-            throw new RuntimeException("Es konnte keine Evaluation des Modells durchgeführt werden: " + e);
-        }
-
-        try {
-            System.out.println(evaluation.toSummaryString("\n---- Evaluation der Testdaten ----\n", false));
-            System.out.println(evaluation.toClassDetailsString());
-            System.out.println(evaluation.toMatrixString());
-        } catch (Exception e) {
-            throw new RuntimeException("Fehler beim Anzeigen der Evaluation: " + e);
-        }
-    }
-
-    public int predictNumber(double[] pixel) {
-
-        if(pixel.length != 784) throw new IllegalArgumentException("Es müssen genau 784 Pixel übergeben werden.");
-
-        Instance instance = new DenseInstance(785);
-
-        for (int i = 0; i < pixel.length; i++) {
-            instance.setValue(i, pixel[i]);
-        }
-
-        double prediction;
-        try {
-            prediction = model.classifyInstance(instance);
-        } catch (Exception e) {
-            throw new RuntimeException("Bei der Prediction ist ein Fehler aufgetreten: " + e);
-        }
-
-        return (int) prediction;
-    }
-
-    private void loadModel(){
-        System.out.println("Lade Modell " + MODEL_PATH);
-
-        try {
-            model = (MultilayerPerceptron) SerializationHelper.read(MODEL_PATH);
-        } catch (Exception e) {
-            throw new RuntimeException(MODEL_PATH + " konnte nicht gelesen werden.");
-        }
     }
 
     private Instances loadData(String path){
-        Instances data;
 
-        System.out.println("Lade Daten " + path);
         try {
-            data = ConverterUtils.DataSource.read(path);
+            Instances data = ConverterUtils.DataSource.read(path);
             data.setClassIndex(data.numAttributes() - 1);
+            return data;
         } catch (Exception e) {
             throw new RuntimeException(path + " konnte nicht gelesen werden.");
+        } finally {
+            System.out.println("Daten geladen: " + path);
         }
 
-        return data;
     }
 
 }
